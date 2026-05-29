0a143b838ce96b3b2e43fd9774d3a97a9593f588
Isaac <isaac.cv123@gmail.com>
2026-05-28 22:04:08 -0400

    Agregar funcionalidad de click en PanelPrincipal

--- src/main/java/frontend/PanelPrincipal.java
+++ src/main/java/frontend/PanelPrincipal.java
@@ -2,12 +2,23 @@ package frontend;
 
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelPrincipal extends JPanel {
     private PanelComprador comprador;
 
     public PanelPrincipal() {
-        this.comprador = new PanelComprador(450, 50, 300, 500);
+        this.comprador = new PanelComprador(450, 20, 300, 530);
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                comprador.click(e.getX(), e.getY());
+
+                repaint();
+            }
+        });
     }
 
     @Override

16cdb84659edafa6f375c79971703ca0155201ac
mtn928 <mcarrasco2025@inf.udec.cl>
2026-06-03 13:38:34 -0400

    dibuja depositos y getters para panelDeposito

--- src/main/java/frontend/PanelDeposito.java
+++ src/main/java/frontend/PanelDeposito.java
@@ -44,11 +44,39 @@ public class PanelDeposito {
     //reposicionar los productos
     public void reposicionar() {
         int offsetY = y + 20;
-        for(int i=0; i < productos.size(); i++) {
-            Producto p = productos.get(i);
-            p.setX(x + (ancho-45)/2); //centrarlo
+        for (Producto p:productos) {
+            p.setX(x + (ancho - 45) / 2); //centrarlo
             p.setY(offsetY);
             offsetY += 62;
         }
+    }
+
+    //dibuja los depositos de los productos
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.GRAY);
+        g.fillRect(x, y, ancho, alto);
+        g.setColor(Color.DARK_GRAY);
+        g.drawRect(x, y, ancho, alto);
+
+        g.setFont(new Font("Arial", Font.BOLD, 8));
+        g.drawString(nombre, x+3, y+12);
+
+        for (Producto p:productos) {
+            p.paintComponent(g);
+        }
+    }
+
+    //getters
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getAncho() {
+        return ancho;
+    }
+    public int getAlto() {
+        return alto;
     }
 }

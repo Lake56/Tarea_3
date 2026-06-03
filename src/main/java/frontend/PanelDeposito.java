a7f68ae6d53baa123f284dc286fcc7157dfbb605
mtn928 <mcarrasco2025@inf.udec.cl>
2026-06-03 13:28:59 -0400

    Metodos para PanelDeposito

--- src/main/java/frontend/PanelDeposito.java
+++ src/main/java/frontend/PanelDeposito.java
@@ -21,4 +21,34 @@ public class PanelDeposito {
         this.productos = new ArrayList<>();
     }
 
+    public void addProducto(Producto p) {
+        productos.add(p);
+        reposicionar();
+    }
+
+    //obtener producto
+    public Producto getProducto() {
+        if (productos.isEmpty()) {
+            return null;
+        }
+        Producto p = productos.remove(0);
+        reposicionar();
+        return p;
+    }
+
+    //verifica si esta vacio o no
+    public boolean estaVacio() {
+        return productos.isEmpty();
+    }
+
+    //reposicionar los productos
+    public void reposicionar() {
+        int offsetY = y + 20;
+        for(int i=0; i < productos.size(); i++) {
+            Producto p = productos.get(i);
+            p.setX(x + (ancho-45)/2); //centrarlo
+            p.setY(offsetY);
+            offsetY += 62;
+        }
+    }
 }

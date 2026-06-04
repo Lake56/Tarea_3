c0fe3b359d66c8a9c20de69fb16b81b8fd67c930
mtn928 <mcarrasco2025@inf.udec.cl>
2026-06-03 22:03:33 -0400

    adicion panelexpendedor a principal

--- src/main/java/frontend/PanelPrincipal.java
+++ src/main/java/frontend/PanelPrincipal.java
@@ -7,13 +7,16 @@ import java.awt.event.MouseEvent;
 
 public class PanelPrincipal extends JPanel {
     private PanelComprador comprador;
+    private PanelExpendedor exp;
 
     public PanelPrincipal() {
+        this.exp = new PanelExpendedor(10, 10, 545, 660, 4);
         this.comprador = new PanelComprador(570, 20, 300, 630);
 
         this.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+                exp.click(e.getX(), e.getY());
                 comprador.click(e.getX(), e.getY());
 
                 repaint();
@@ -25,6 +28,7 @@ public class PanelPrincipal extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        exp.paintComponent(g);
         if(comprador != null) {
             comprador.paintComponent(g);
         }

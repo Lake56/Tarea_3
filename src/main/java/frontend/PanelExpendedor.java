ade9c34c32843dc846f17ff4d29bfb80df1605e5
mtn928 <mcarrasco2025@inf.udec.cl>
2026-06-03 21:20:23 -0400

    Inicializa depositos con stock

--- src/main/java/frontend/PanelExpendedor.java
+++ src/main/java/frontend/PanelExpendedor.java
@@ -36,7 +36,7 @@ public class PanelExpendedor {
         int margen = 15;
         int brecha = 10;
 
-        depCoca = new PanelDeposito(x+ margen,y + 35, depAncho, depAlto, "CocaCola");
+        depCoca = new PanelDeposito(x+ margen,y + 35, depAncho, depAlto, "Coca Cola");
         depSprite = new PanelDeposito(x+ margen +(depAncho + brecha),y + 35, depAncho, depAlto, "Sprite");
         depFanta = new PanelDeposito(x+ margen +(depAncho + brecha) * 2, y + 35, depAncho, depAlto, "Fanta");
         depSnickers = new PanelDeposito(x+ margen +(depAncho + brecha) * 3, y + 35, depAncho, depAlto, "Snickers");
@@ -47,4 +47,37 @@ public class PanelExpendedor {
 
     }
 
+    //carga con stock los depositos
+    private void inicializarProductos() {
+
+        for (int i = 0; i < numProductos; i++) {
+            depCoca.addProducto(new Producto("Coca Cola", "n:" + contadorSerie++));
+            depSprite.addProducto(new Producto("Sprite", "n:" + contadorSerie++));
+            depFanta.addProducto(new Producto("Fanta", "n:" + contadorSerie++));
+            depSnickers.addProducto(new Producto("Snickers", "n:" + contadorSerie++));
+            depSuper8.addProducto(new Producto("Super8", "n:" + contadorSerie++));
+        }
+    }
+    private void rellenarVacios() {
+        if (depCoca.estaVacio()) {
+            for (int i =0; i <numProductos; i++)
+                depCoca.addProducto(new Producto("Coca Cola", "n:" + contadorSerie++));
+        }
+        else if (depSprite.estaVacio()) {
+            for (int i =0; i <numProductos; i++)
+                depSprite.addProducto(new Producto("Sprite", "n:" + contadorSerie++));
+        }
+        else if (depFanta.estaVacio()) {
+            for (int i =0; i <numProductos; i++)
+                depFanta.addProducto(new Producto("Fanta", "n:" + contadorSerie++));
+        }
+        else if (depSnickers.estaVacio()) {
+            for (int i =0; i <numProductos; i++)
+                depSnickers.addProducto(new Producto("Snickers", "n:" + contadorSerie++));
+        }
+        else if (depSuper8.estaVacio()) {
+            for (int i =0; i <numProductos; i++)
+                depSuper8.addProducto(new Producto("Super8", "n:" + contadorSerie++));
+        }
+    }
 }

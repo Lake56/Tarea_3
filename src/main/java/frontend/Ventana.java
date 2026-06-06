78b13c6b5a04368320cf6c0a707f5181c0455bc1
Eltigre <bastiton@gmail.com>
2026-06-06 02:44:49 -0400

    Se documenta la clase Ventana

--- src/main/java/frontend/Ventana.java
+++ src/main/java/frontend/Ventana.java
@@ -2,7 +2,17 @@ package frontend;
 
 import javax.swing.*;
 
+/**
+ * Ventana principal de la aplicacion
+ * inicializa el panel principal para las interacciones
+ * entre el comprador y el expendedor
+ */
+
 public class Ventana extends JFrame {
+    /**
+     * Crea la ventana principal y panel principal
+     * Configura su tamaño y posicion
+     */
     public Ventana(){
         super("Maquina Expendedora");
 

b5ea2eaa1e131e497dc70f83c583274bbc4fa57a
Eltigre <bastiton@gmail.com>
2026-06-06 02:09:11 -0400

    Se documenta la clase PanelDeposito

--- src/main/java/frontend/PanelDeposito.java
+++ src/main/java/frontend/PanelDeposito.java
@@ -4,22 +4,39 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * Representa de forma grafica el deposito de productos
+ * Permite almacenar, retirar y visualizar los productos dentro
+ * del expendedor.
+ */
 public class PanelDeposito extends JPanel {
     private String nombre;
     private ArrayList<Producto> productos;
 
+    /**
+     * Se crea un deposito con su nombre
+     * @param nombre del deposito
+     */
     public PanelDeposito(String nombre) {
         this.nombre = nombre;
         this.productos = new ArrayList<>();
     }
 
+    /**
+     * Se añaden los productos al deposito
+     * y acutaliza su posicion
+     * @param p producto a agregar
+     */
     public void addProducto(Producto p) {
         productos.add(p);
         reposicionar();
         repaint();
     }
 
-    //obtener producto
+    /**
+     * Retira el primero producto del deposito
+     * @return producto o null si el deposito esta vacio
+     */
     public Producto getProducto() {
         if (productos.isEmpty()) {
             return null;
@@ -30,12 +47,18 @@ public class PanelDeposito extends JPanel {
         return p;
     }
 
-    //verifica si esta vacio o no
+    /**
+     * Verifica que el deposito tiene productos
+     * @return true si el deposito esta vacio
+     * false si el deposito tiene productos
+     */
     public boolean estaVacio() {
         return productos.isEmpty();
     }
 
-    //reposicionar los productos
+    /**
+     * Recalula las posiciones del producto respecto al eje y
+     */
     public void reposicionar() {
         int offsetY = 20;
 
@@ -45,7 +68,10 @@ public class PanelDeposito extends JPanel {
         }
     }
 
-    //dibuja los depositos de los productos
+    /**
+     * Dibuja el deposito y sus productos
+     * @param g contexto grafico del dibujo
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);

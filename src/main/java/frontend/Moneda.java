790f1557e2ea7f9cd313d80902bf2c568313b9b4
Eltigre <bastiton@gmail.com>
2026-06-06 01:38:44 -0400

    Se documenta la clase Moneda

--- src/main/java/frontend/Moneda.java
+++ src/main/java/frontend/Moneda.java
@@ -3,7 +3,12 @@ package frontend;
 import javax.swing.*;
 import java.awt.*;
 
-public class Moneda {
+public class Moneda{
+    /**
+     * Se representa de forma grafica una moneda
+     * cargando una imagen con su valor
+     * y numero de serie correspondiente
+     */
     private int valor;
     private String serie;
     private int x;
@@ -11,6 +16,11 @@ public class Moneda {
 
     private Image imagen;
 
+    /**
+     * Contruye la moneda grafica con su valor y serie
+     * @param valor de la moneda
+     * @param serie de la moneda
+     */
     public Moneda(int valor, String serie) {
         this.valor = valor;
         this.serie = serie;
@@ -18,6 +28,12 @@ public class Moneda {
         cargarImagen();
     }
 
+    /**
+     * Carga la imagen de la moneda con
+     * su correspondiente valor
+     * Verifica que la imagen exista
+     * en caso de que no exista se ocupa una representacion grafica alternativa
+     */
     public void cargarImagen() {
         String ruta = "";
 
@@ -43,6 +59,11 @@ public class Moneda {
         }
     }
 
+    /**
+     * Dibuja la Moneda en la ventana
+     * En caso de que no exista se dibuja una representacion
+     * @param g contexto grafico del dibujo
+     */
     public void paintComponent(Graphics g) {
         if(imagen != null) {
             g.drawImage(imagen, x, y, null);
@@ -58,14 +79,26 @@ public class Moneda {
         g.drawString(serie, x + 5, y + 55);
     }
 
+    /**
+     * Actualiza la coordenada x de la moneda
+     * @param x nueva posicion
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * Actualiza la coordenada y de la moneda
+     * @param y nueva posicion
+     */
     public void setY(int y) {
         this.y = y;
     }
 
+    /**
+     * Obtiene el valor de la moneda
+     * @return valor de la moneda
+     */
     public int getValor() {
         return valor;
     }
